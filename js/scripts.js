c4ade5cff93f9c5196f94ad673292afd81a66f8d
Kaila Goff <kaila.goff@gmail.com>
2018-01-26 11:58:38 -0800

    math not adding up properly, but functionality is closer

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,35 @@
-function Pizza(toppings, size) {
-  this.toppings = toppings;
+function Pizza(size, toppings) {
   this.size = size;
+  this.toppings = toppings;
 }
 
 Pizza.prototype.price = function() {
   var cost = 10;
-  if (this.size === "small") {
-    cost -= 2;//+ (this.toppings.length);
-  } else if (this.size === "large") {
-    cost += 5; // + (this.toppings.length);
+
+  if (this.sizePizza === "small") {
+    cost -= 2 + (this.toppings.length + 3);
+  } else if (this.sizePizza === "large") {
+    cost += 2 + (this.toppings.length + 3);
+  } else {
+    cost = cost + (this.toppings.length + 3);
   }
     return cost;
 };
 
 $(document).ready(function() {
-  $("form#order-form").submit(function(event) {
+  $("form#orderForm").submit(function(event) {
     event.preventDefault();
 
-    var sizeInput = $("input[value=size]:checked").val();
+    var sizeInput = $("select#size").val();
+    console.log($("select#size").val());
     var toppingsInput = [];
+
     $("input:checked").each(function() {
       toppingsInput.push($(this).val());
     });
 
     var newPizza = new Pizza(sizeInput, toppingsInput);
-      console.log("created new object")
+      console.log(newPizza)
 
     $("ul#pizzaReciept").append("<li>$" + newPizza.price() + "</li>");
 
